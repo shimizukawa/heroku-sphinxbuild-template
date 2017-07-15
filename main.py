d244641fa41564a46e4aab1956f30c820f9ed418
shimizukawa <shimizukawa@gmail.com>
2017-07-15 16:50:49 +0900

    improve script

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import os
 import bottle
 
 ROOT = os.path.join(os.environ.get('HTML_PATH', '.'))
-AUTH = os.environ.get('BASIC_AUTH', ':').split(':', 1)
+AUTH = os.environ.get('BASIC_AUTH', None)
 PORT = int(os.environ.get('PORT', '8080'))
-
-app = bottle.Bottle()
 
 
 def check(username, password):
-    return [username, password] == AUTH
+    return ':'.join([username, password]) == AUTH
 
 
-@app.route('<path:path>')
-@bottle.auth_basic(check)
 def server_static(path):
     if path.endswith('/'):
         path += 'index.html'
     return bottle.static_file(path, root=ROOT)
 
+if AUTH is not None:
+    server_static = bottle.auth_basic(check)(server_static)
+
+server_static = bottle.route('<path:path>')(server_static)
 
 if __name__ == '__main__':
-    bottle.run(app, host='0.0.0.0', port=PORT)
+    bottle.run(host='0.0.0.0', port=PORT)
